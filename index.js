badc3633617827b523e4d87ee2ed38164ca0a869
romario <romarioagros66@gmail.com>
2021-10-23 19:59:25 +0300

    чтение файла

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-var events = require("events");
-var util = require("util");
-
-var Cars = function (model) {
-  this.model = model;
-};
-
-util.inherits(Cars,events.EventEmitter)
-
-var bmw = new Cars("BMW");
-var audi = new Cars("Audi");
-var volvo = new Cars("Volvo");
-
-var cars = [bmw , audi,volvo];
-cars.forEach(function(car){
-    car.on("speed", function(text){
-        console.log(car.model + " speeed is " + text);
-    });
-});
-
-bmw.emit("speed", "254.5 km");
-volvo.emit("speed", "180 km");
+var fs = require("fs");
+ 
+var  file_readed = fs.readFileSync("text.txt", "utf8");
+console.log(file_readed); 
