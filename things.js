cd3d06548d8170b268aaaf254202132c5c333cb3
romario <romarioagros66@gmail.com>
2021-10-23 19:29:29 +0300

    третий способ экспорта

--- things.js
+++ things.js
@@ -1,13 +1,19 @@
-module.exports.array_counter= function (arr ){
+var array_counter = function (arr ){
     return "В массиве находиться " + arr.length + " элементов!";
 };
 
 //console.log(array_counter([1,7,99,8,45]));
-module.exports.multiply = function(x,y ){
+var multiply = function(x,y ){
     return `${x} умножить на ${y} Равно ${x*y} `
 }
 
-module.exports.some_value = 451;
+var some_value = 451;
 
+module.exports = {
+    array_counter:array_counter,
+    multiply:multiply,
+    some_value:some_value,
+
+};
 
 
