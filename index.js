477c6c6bb2f2a4db74a9ceca41ae84e5f19fc1b8
romario <romarioagros66@gmail.com>
2021-10-23 20:02:39 +0300

    запись файла

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var fs = require("fs");
  
 var  file_readed = fs.readFileSync("text.txt", "utf8");
+var message = "Привет мир !\n " + file_readed;
+fs.writeFileSync('some_new.txt',message );
 console.log(file_readed); 
