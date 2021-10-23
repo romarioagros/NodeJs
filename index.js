f508b78ce7ebe5b618b5dc4136f40bd5d36c9709
romario <romarioagros66@gmail.com>
2021-10-23 20:09:25 +0300

    асинхронное чтение и запись

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var fs = require("fs");
- 
-var  file_readed = fs.readFileSync("text.txt", "utf8");
-var message = "Привет мир !\n " + file_readed;
-fs.writeFileSync('some_new.txt',message );
-console.log(file_readed); 
+ // асинхронное прочтение файла
+fs.readFile("text.txt", "utf8", function(err,data){
+    console.log(data);
+});
+
+fs.writeFile("some.txt", 'Hi its me', function(err,data){
+    
+});
+
+console.log('test');
