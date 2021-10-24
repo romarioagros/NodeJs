677d90990cd8895a087ab92d6607f6beb969e9cf
romario <romarioagros66@gmail.com>
2021-10-24 19:26:02 +0300

    выводим html на сайт

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var fs = require('fs')
-
+var http = require('http');
 
 var myReadShort = fs.createReadStream(__dirname + '/voyna-i-mir-tom-1.txt' , 'utf8');
 var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
 
 
 myReadShort.pipe(myWriteShort);
+
+
+
+var server = http.createServer(function(req,res){
+    console.log("Url страницы"+ req.url);
+    res.writeHead(200,{'Content-Type':"text/html; charset=utf-8"});
+    var myReadShort = fs.createReadStream(__dirname + '/index.html' , 'utf8');
+    //var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
+
+
+    myReadShort.pipe(res);
+  
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('мы отслеживаем порт 3000');
