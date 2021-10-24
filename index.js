41b96b10e6346bbaea5dd06d6ec14e2e4c6899cc
romario <romarioagros66@gmail.com>
2021-10-24 09:09:06 +0300

    удаление файла и папки

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var fs = require('fs');
-//fs.unlink('some.txt', function(){})
+fs.unlink('./new-one/some.txt', function(){})
 //создание папки
 //fs.mkdirSync('new-one');
 //удаление папки
 //fs.rmdirSync('new-one');
 
+fs.rmdir('new-one', function(){});
+
+
 //создание папки асинхронно
-fs.mkdir('new-one',function(){
-    fs.writeFile('./new-one/some.txt', 'привет привет привет', function(){
+/* fs.mkdir('new-one',function(){;
+    fs.writeFile('./new-one/some.txt', 'привет привет привет', function(){;
         console.log("все сработало");
-    })
-});
+    });
+}); */
