eca54729f5aeef6819065eaff3db322794d78ec5
romario <romarioagros66@gmail.com>
2021-10-24 09:06:35 +0300

    создание и удаление директорий урок 7

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var fs = require('fs');
-fs.unlink('some.txt', function(){})
+//fs.unlink('some.txt', function(){})
+//создание папки
+//fs.mkdirSync('new-one');
+//удаление папки
+//fs.rmdirSync('new-one');
+
+//создание папки асинхронно
+fs.mkdir('new-one',function(){
+    fs.writeFile('./new-one/some.txt', 'привет привет привет', function(){
+        console.log("все сработало");
+    })
+});
