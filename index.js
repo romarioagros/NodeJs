c89f42cab5bb9ce318e6fa9d6ed4db19179e8302
romario <romarioagros66@gmail.com>
2021-10-24 19:45:11 +0300

    ввывод json

--- index.js
+++ index.js
@@ -11,14 +11,19 @@ myReadShort.pipe(myWriteShort);
 
 var server = http.createServer(function(req,res){
     console.log("Url страницы"+ req.url);
-    res.writeHead(200,{'Content-Type':"text/html; charset=utf-8"});
-    var myReadShort = fs.createReadStream(__dirname + '/index.html' , 'utf8');
+    res.writeHead(200,{'Content-Type':"application/json; charset=utf-8"});
+    var obj = {
+        model:"Audi",
+        speed :  '234.5',
+        wheels : 4
+    }
+    //var myReadShort = fs.createReadStream(__dirname + '/index.html' , 'utf8');
     //var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
 
 
-    myReadShort.pipe(res);
+   res.end(JSON.stringify(obj));
   
 });
 
-server.listen(3000,'127.0.0.1');
+server.listen(80,'localhost');
 console.log('мы отслеживаем порт 3000');
