029665ec3cfbe7fdc685ea84de0eed9fc9aaccfd
romario <romarioagros66@gmail.com>
2021-10-24 16:22:31 +0300

    локальный сервер урок 8

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req,res){
+    console.log("Url страницы"+ req.url);
     res.writeHead(200,{'Content-Type':"text/plain; charset=utf-8"});
     res.end("привет мир!");
 });
