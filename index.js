6d2c2c5a1deee30954525b42a7b5fb54b44bfde9
romario <romarioagros66@gmail.com>
2021-10-30 13:47:40 +0300

    использование шаблонизатора

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var express = require("express");
 
+
 var app = express();
+app.set('view engine', 'ejs');
+
+
 
 app.get("/", function (req, res) {
-    res.send("this is home");
+    res.sendFile(__dirname+ "/index.html");
 });
 
-app.get("/news", function (req, res) {
-    res.send("this is news");
+app.get("/about", function (req, res) {
+    res.sendFile(__dirname +  "/about.html");
 });
 
-app.get("/news/:name/:id", function (req, res) {
-    res.send("this is id " + req.params.id + req.params.id);
+app.get("/news/:id", function (req, res) {
+    var obj = {title:"Новость " , id:4,paragraphs:['Параграф','Обычный' , 'текст', 'числа: 2 ,4,6', 99 ]}
+    res.render("news", { newsId: req.params.id,newParam:234, obj:obj}   );
 });
 
 
